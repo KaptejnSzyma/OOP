35ec30c22a6218f48e01bc86882b4cf8829ac3be
Szymek <szymon.rzadca@gmail.com>
2018-06-07 10:20:53 +0200

    using replacement fields features to simplify printing contents of class instances

--- oop.py
+++ oop.py
@@ -16,3 +16,5 @@ print(kenwood.price)
 hamilton = Kettle("Hamilton", 14.55)
 
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+
+print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
