2a26ff90c5be086f07ef9d184ac134f573c3815d
Szymek <szymon.rzadca@gmail.com>
2018-06-07 10:30:46 +0200

    using various methods to switch on the kettle

--- oop.py
+++ oop.py
@@ -5,6 +5,9 @@ class Kettle(object):
         self.price = price
         self.on = False
 
+    def switch_on(self):
+        self.on = True
+
 
 kenwood = Kettle("Kenwood", 8.99)
 print(kenwood.make)
@@ -18,3 +21,11 @@ hamilton = Kettle("Hamilton", 14.55)
 print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
 
 print("Models: {0.make} = {0.price}, {1.make} = {1.price}".format(kenwood, hamilton))
+
+
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+
+Kettle.switch_on(kenwood)
+print(kenwood.on)
