c94c617f002e8fb830966fd4bd4758fd0e7c20d7
Szymek <szymon.rzadca@gmail.com>
2018-06-07 11:00:22 +0200

    creating customer, depositing and withdrawing money

--- accounts.py
+++ accounts.py
@@ -1,10 +1,10 @@
 class Account:
-    """Simple account class with balance"""
+    """ Simple account class with balance """
 
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
-        print("Account created for" + self.name)
+        print("Account created for " + self.name)
 
     def deposit(self, amount):
         if amount > 0:
@@ -16,3 +16,12 @@ class Account:
 
     def show_balance(self):
         print("Balance is {}".format(self.balance))
+
+
+if __name__ == '__main__':
+    tim = Account("Tim", 0)
+    tim.show_balance()
+    tim.deposit(1000)
+    tim.show_balance()
+    tim.withdraw(500)
+    tim.show_balance()
