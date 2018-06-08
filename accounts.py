923dc5e6688192bfb6de094881e090a450255045
Szymek <szymon.rzadca@gmail.com>
2018-06-08 10:18:07 +0200

    showcasing name mangling

--- accounts.py
+++ accounts.py
@@ -12,27 +12,27 @@ class Account:
 
     def __init__(self, name, balance):
         self._name = name
-        self._balance = balance
+        self.__balance = balance
         self._transaction_list = [(Account._current_time(), balance)]
         print("Account created for " + self._name)
         self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self._balance += amount
+            self.__balance += amount
             self.show_balance()
             self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self._balance:
-            self._balance -= amount
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
             self._transaction_list.append((Account._current_time(), -amount))
         else:
             print("The amount must be greater than zero and no more than your current account balance")
         self.show_balance()
 
     def show_balance(self):
-        print("Balance is {}".format(self._balance))
+        print("Balance is {}".format(self.__balance))
 
     def show_transactions(self):
         for date, amount in self._transaction_list:
@@ -57,8 +57,9 @@ if __name__ == '__main__':
     tim.show_transactions()
 
     steph = Account("Steph", 800)
-    steph._balance = 200
+    steph.__balance = 200
     steph.deposit(100)
     steph.withdraw(200)
     steph.show_transactions()
     steph.show_balance()
+    print(steph.__dict__)
