bc8edc3c73d000f5c7a7ecfe6670e383d4bc1f79
Szymek <szymon.rzadca@gmail.com>
2018-06-11 10:57:12 +0200

    creating find object function to enhance loading of data

--- song.py
+++ song.py
@@ -80,6 +80,14 @@ class Artist:
         self.albums.append(album)
 
 
+def find_object(field, object_list):
+    """Check the 'object_list' to see if an object with a 'name' attribute equal to 'field' exists, return it if so"""
+    for item in object_list:
+        if item.name == field:
+            return item
+    return None
+
+
 def load_data():
     new_artist = None
     new_album = None
